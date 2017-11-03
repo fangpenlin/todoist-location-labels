2b0b466fcfef599ff6b8e4f009f2b39271d30444
Fang-Pen Lin <hello@fangpenlin.com>
2017-11-02 20:42:14 -0700

    Update layout

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 import sys
 import os
 import uuid
+import urllib.parse
+import base64
 
+import todoist
 import requests
 from flask import Flask
 from flask import request
 from flask import json
+from flask import render_template
+from flask import redirect
+from flask import session
+from flask import abort
+from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -14,7 +22,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
     'DATABASE_URL',
     'sqlite:////tmp/test.db'
 )
+app.secret_key = os.environ['APP_SECRET_KEY']
 db = SQLAlchemy(app)
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
 
 
 class User(db.Model):
@@ -38,9 +49,61 @@ class LocationLabel(db.Model):
     radius = db.Column(db.Float, nullable=False)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    user_id = session.get('user_id')
+    kwargs = {}
+    if user_id is not None:
+        user = User.query.get(user_id)
+        labels = requests.get(
+            'https://beta.todoist.com/API/v8/labels',
+            params=dict(token=user.oauth_token)
+        ).json()
+        kwargs['labels'] = labels
+        api = todoist.TodoistAPI(user.oauth_token)
+        kwargs['user_full_name'] = api.user.get('full_name')
+    return render_template('index.html', **kwargs)
+
+
+@app.route('/authorize')
+def authorize():
+    state = base64.b64encode(os.urandom(32)).decode('utf8')
+    session['oauth_secret_state'] = state
+    return redirect(
+        'https://todoist.com/oauth/authorize?' + urllib.parse.urlencode(dict(
+            client_id=client_id,
+            scope='data:read_write',
+            state=state,
+        ))
+    )
+
+
+@app.route('/oauth/redirect')
+def oauth_redirect():
+    state = session['oauth_secret_state']
+    if request.args.get('state') != state:
+        return abort(401)
+    code = request.args.get('code')
+    if not code:
+        return abort(400)
+    resp = requests.post('https://todoist.com/oauth/access_token', data=dict(
+        client_id=client_id,
+        client_secret=client_secret,
+        code=code,
+        redirect_uri=url_for('authorize', _external=True),
+    ))
+    resp.raise_for_status()
+    access_token = resp.json()['access_token']
+    api = todoist.TodoistAPI(access_token)
+    user_id = api.user.get_id()
+    user = User.query.get(user_id)
+    if user is None:
+        user = User(id=user_id, oauth_token=access_token)
+    else:
+        user.oauth_token = access_token
+    db.session.commit()
+    session['user_id'] = user.id
+    return redirect(url_for('index'))
 
 
 @app.route('/webhook', methods=['POST'])
@@ -51,6 +114,7 @@ def webhook():
     initiator = event['initiator']
     event_data = event['event_data']
     user = User.query.get(initiator['id'])
+    api = todoist.TodoistAPI(user.oauth_token)
     for label_id in event_data['labels']:
         loc_labels = user.location_labels.filter_by(label_id=label_id).all()
         if not loc_labels:
@@ -58,28 +122,17 @@ def webhook():
         for loc_label in loc_labels:
             temp_id = uuid.uuid4().hex
             req_uuid = uuid.uuid4().hex
-            resp = requests.post('https://todoist.com/api/v7/sync', data=dict(
-                token=user.oauth_token,
-                commands=json.dumps([
-                    dict(
-                        type='reminder_add',
-                        temp_id=temp_id,
-                        uuid=req_uuid,
-                        args=dict(
-                            item_id=event_data['id'],
-                            type='location',
-                            name=loc_label.name,
-                            loc_long=loc_label.long,
-                            loc_lat=loc_label.lat,
-                            loc_trigger=loc_label.loc_trigger,
-                            radius=loc_label.radius 
-                        )
-                    )
-                ])
-            ))
-            print(resp.json())
-            resp.raise_for_status()
-    return ''
+            api.reminders.add(
+                event_data['id'],
+                type='location',
+                name=loc_label.name,
+                loc_lat=loc_label.lat,
+                loc_long=loc_label.long,
+                loc_trigger=loc_label.loc_trigger,
+                radius=loc_label.radius 
+            )
+    api.commit()
+    return 'ok'
 
 if __name__ == '__main__':
     if len(sys.argv) >= 2 and sys.argv[1] == 'initdb':
